290dc5cdd06c585187c858617ef6e40d453eaa13
김우진 <dnwls488@naver.com>
2022-08-25 01:36:22 +0900

    dto 를 통한 resposne 및 예외처리

--- src/main/java/com/fc/sns/controller/UserController.java
+++ src/main/java/com/fc/sns/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.fc.sns.controller;
 
+import com.fc.sns.controller.request.UserJoinRequest;
+import com.fc.sns.controller.response.Response;
+import com.fc.sns.controller.response.UserJoinResponse;
+import com.fc.sns.model.User;
 import com.fc.sns.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +19,9 @@ public class UserController {
     private final UserService userService;
 
     // TODO : implement
-    @PostMapping
-    public void join() {
-        userService.join("", "");
+    @PostMapping("/join")
+    public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
+        User user = userService.join(request.getUserName(), request.getPassword());
+        return Response.success(UserJoinResponse.fromUser(user));
     }
 }
---
+++ src/main/java/com/fc/sns/controller/response/UserJoinResponse.java
@@ -0,0 +1,23 @@
+package com.fc.sns.controller.response;
+
+import com.fc.sns.model.User;
+import com.fc.sns.model.UserRole;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserJoinResponse {
+
+    private Long id;
+    private String userName;
+    private UserRole role;
+
+    public static UserJoinResponse fromUser(User user) {
+        return new UserJoinResponse(
+                user.getId(),
+                user.getUserName(),
+                user.getUserRole()
+        );
+    }
+
+}
