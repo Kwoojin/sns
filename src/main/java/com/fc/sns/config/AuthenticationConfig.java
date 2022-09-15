37c7acdde047f14242826790b039a169b3f078cf
김우진 <dnwls488@naver.com>
2022-09-16 01:38:53 +0900

    인증 관련 기능 추가

---
+++ src/main/java/com/fc/sns/config/AuthenticationConfig.java
@@ -0,0 +1,41 @@
+package com.fc.sns.config;
+
+import com.fc.sns.config.filter.JwtTokenFilter;
+import com.fc.sns.exception.CustomAuthenticationEntryPoint;
+import com.fc.sns.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthenticationConfig {
+
+    private final UserService userService;
+
+    @Value("${jwt.secret-key}")
+    private String key;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeRequests()
+                .mvcMatchers("/api/*/users/join", "/api/*/users/login").permitAll()
+                .mvcMatchers("/api/**").authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JwtTokenFilter(key, userService), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .and().build();
+    }
+
+}
--- src/main/java/com/fc/sns/config/SecurityConfig.java
+++ src/main/java/com/fc/sns/config/SecurityConfig.java
@@ -2,10 +2,7 @@ package com.fc.sns.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -13,23 +10,6 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder encoderPasswordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf().disable()
-                .authorizeRequests()
-                .mvcMatchers("/api/*/users/join", "/api/*/users/login").permitAll()
-                .mvcMatchers("/api/**").authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .build();
-//                TODO
-//                .exceptionHandling()
-//                .authenticationEntryPoint()
     }
 
 }
