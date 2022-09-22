ef210d23c34240bb9a180409ab5fa81cc33c7561
김우진 <dnwls488@naver.com>
2022-09-22 23:55:48 +0900

    front_end 요청 관련 Security 제외

--- src/main/java/com/fc/sns/config/AuthenticationConfig.java
+++ src/main/java/com/fc/sns/config/AuthenticationConfig.java
@@ -5,9 +5,11 @@ import com.fc.sns.exception.CustomAuthenticationEntryPoint;
 import com.fc.sns.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -20,6 +22,11 @@ public class AuthenticationConfig {
 
     @Value("${jwt.secret-key}")
     private String key;
+
+    @Bean
+    public void securityFilterChain(WebSecurity web) throws Exception {
+        web.ignoring().regexMatchers("^(?!/api/).*");
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
